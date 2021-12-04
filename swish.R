fa7c74949aae0dbbd904fa585f32c2b54dfc5f50
mikelove <michaelisaiahlove@gmail.com>
2021-12-04 05:58:47 -0500

    new

--- swish.R
+++ swish.R
@@ -1,18 +1,21 @@
 suppressPackageStartupMessages(library(SummarizedExperiment))
 library(tximeta)
 library(devtools)
-load_all("../fishpond/fishpond")
+load_all("../../fishpond/fishpond")
 
 # import tx2gene
-dir <- "boot_quants"
+dir <- "../ase-sim/quants"
 samps <- list.files(dir)
 files <- file.path(dir, samps, "quant.sf")
 
-t2g <- read.table("tx2gene.oracle.tsv", header=TRUE)
-table(table(sub("_[MP]","",t2g[,1]))) # check that we have 2x txps
-t2g$txp <- sub("_.*","",t2g$txp)
-t2g$gene <- sub("_.*","",t2g$gene)
-t2g <- t2g[!duplicated(t2g$txp),]
+if (FALSE) {
+  load("../ase-sim/granges.rda")
+  mcols(txps)$txp_groups <- paste0(txps$gene_id, "-", round(txps$abundance,2))
+  t2g <- data.frame(txp=c(names(txps)), gene=c(txps$txp_groups))
+  write.table(t2g, file="t2g.oracle.tsv", quote=FALSE, row.names=FALSE)
+}
+
+t2g <- read.table("t2g.oracle.tsv", header=TRUE)
 
 s <- paste0("samp",seq_along(files))
 coldata <- data.frame(files=files, names=s, sample=s)
@@ -21,12 +24,19 @@ wide <- importAllelicCounts(coldata, a1="P", a2="M",
                             tx2gene=t2g,
                             ignoreAfterBar=TRUE)
 
+# no summarization
+wide <- importAllelicCounts(coldata, a1="P", a2="M",
+                            format="wide",
+                            ignoreAfterBar=TRUE)
+
 assayNames(wide)
 
-save(wide, file="se.oracle.rda")
-
+#save(wide, file="se.oracle.rda")
+save(wide, file="se.txp.rda")
 # load object
-load("/proj/milovelab/wu/bulk-ase/ase-analysis/Bootstrap_Analysis/wide.oracle.b+.rda")
+#load("/proj/milovelab/wu/bulk-ase/ase-analysis/Bootstrap_Analysis/wide.txp.b+.rda")
+#load("/proj/milovelab/wu/bulk-ase/ase-analysis/Bootstrap_Analysis/wide.oracle.b+.rda")
+#load("/proj/milovelab/wu/bulk-ase/ase-analysis/Bootstrap_Analysis/wide.oracle.b.rda")
 
 y <- wide
 y <- labelKeep(y)
@@ -37,54 +47,41 @@ y <- swish(y, x="allele", pair="sample")
 table(mcols(y)$qvalue < .01)
 hist(mcols(y)$pvalue, col="grey")
 
+# oracle
 sig_txps <- rownames(y)[mcols(y)$qvalue < 0.01]
 table(grepl("-2$", sig_txps))
+table(ai=!grepl("-2$", rownames(y)), sig=mcols(y)$qvalue < 0.01)
+prop.table(table(ai=!grepl("-2$", rownames(y)), sig=mcols(y)$qvalue < 0.01), 1)
 
-grep("-2$", sig_txps)
-fp <- sub("-2","",sig_txps[grep("-2$", sig_txps)])
-table(grepl("-2$", sig_txps))
+# txp-level
+mcols(y)$abund <- mcols(txps)[rownames(y),"abundance"]
 
-# check repeat status
-library(rtracklayer)
-rep <- import("simpleRepeat_dm6_Aug2014.bed.gz")
-seqlevelsStyle(rep) <- "NCBI"
-rep <- rep[seqnames(rep) %in% c("2L","2R","3L","3R","4","X")]
-rep <- keepStandardChromosomes(rep, pruning.mode="coarse")
-library(AnnotationHub)
-ah <- AnnotationHub()
-release <- 100
-q <- query(ah, c("EnsDb",release,"Drosophila"))
-library(ensembldb)
-edb <- q[[1]]
-g <- exonsBy(edb, by="gene")
-g <- keepStandardChromosomes(g, pruning.mode="coarse")
-g_fp <- g[fp]
-rep_size <- 50
-big_rep <- rep[width(rep) >= rep_size]
-table(overlapsAny(g_fp, big_rep, minoverlap=rep_size))
-table(overlapsAny(g, big_rep, minoverlap=rep_size))
-g_fp_extra <- g_fp[!overlapsAny(g_fp, big_rep, minoverlap=rep_size)]
-names(g_fp_extra)
+sig <- mcols(y)$qvalue < 0.1
+ai <- mcols(y)$abund != 2
+table(ai, sig)
 
-g0 <- genes(edb)
-dat <- cbind(as.data.frame(g0[names(g_fp_extra)]),
-             as.data.frame(mcols(y)[paste0(names(g_fp_extra),"-2"),]))
-dat[dat$pvalue < .0001,]
-table(dat[dat$pvalue < .0001,]$gene_biotype == "pseudogene")
 
 ###
 
-hist(mcols(y)[sig_txps[grep("-2$", sig_txps)],]$log2FC, breaks=20, border="white", col="grey")
+# check structure in plot
 
-zzz <- mcols(y)[sig_txps[grep("-2$", sig_txps)],]
-zzz <- zzz[order(zzz$pvalue),]
-head(zzz,10)
-
-txps[txps$gene_id == "FBgn0003372"]
-
-y$s <- c(1:10,1:10)
+load("se.txp.rda")
+y <- wide
+y <- labelKeep(y)
+y <- y[mcols(y)$keep,]
 y <- computeInfRV(y)
-plotInfReps(y, idx="FBgn0001234-2", x="s", cov="allele", legend=TRUE, legendPos="bottom", xlab="sample")
-plotInfReps(y, idx="FBgn0003372-2", x="s", cov="allele", legend=TRUE, legendPos="bottom", xlab="sample")
-plotInfReps(y, idx="FBgn0003373-2", x="s", cov="allele", legend=TRUE, legendPos="bottom", xlab="sample")
-plotInfReps(y, idx="FBgn0003943-2", x="s", cov="allele", legend=TRUE, legendPos="bottom", xlab="sample")
+#y2 <- y[1:2000,]
+out <- swish(y, x="allele", pair="sample", returnNull=TRUE)
+sigma <- sqrt(rowVars(out$nulls))
+mcols(y)$mean <- rowMeans(assay(y))
+mcols(y)$abund <- mcols(txps)[rownames(y),"abundance"]
+plot(log10(mcols(y)$meanInfRV+.1), log10(sigma), cex=.2, col=ifelse(mcols(y2)$abund == 2,1,2))
+plot(log10(mcols(y)$meanInfRV+.1), log10(sigma), cex=.2)
+idx <- rowSums(assay(y) == 0) == 1
+plot(log10(mcols(y)$meanInfRV+.1), log10(sigma), cex=1, col=ifelse(idx, 2, 1), pch=20)
+plot(log10(mcols(y)$meanInfRV+.1), log10(mcols(y)$mean), cex=1, col=ifelse(idx, 2, 1), pch=20)
+idx2 <- rowMedians(abs(assay(y)[,1:10] - assay(y)[,11:20])) < 1
+plot(log10(mcols(y)$meanInfRV+.1), log10(mcols(y)$mean), cex=1, col=ifelse(idx2, 2, 1), pch=20)
+plot(log10(mcols(y)$meanInfRV+.1), log10(sigma), cex=1, col=ifelse(idx2, 2, 1), pch=20)
+
+idx3 <- rownames(y)[idx2]
