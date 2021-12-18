8edd5304e1a242ca6740330f240941806eb610ef
mikelove <michaelisaiahlove@gmail.com>
2021-12-18 12:36:16 -0500

    more

--- betabin.R
+++ betabin.R
@@ -1,15 +1,31 @@
 suppressPackageStartupMessages(library(SummarizedExperiment))
-library(tximeta)
 library(fishpond)
-
+library(apeglm)
+library(locfdr)
 types <- c("txp","oracle","gene","tss")
 for (t in types) {
   load(file=paste0("data/se_",t,".rda"))
   y <- wide
+  assays(y) <- assays(y)["counts"]
+  rm(wide)
   y <- labelKeep(y)
-  y <- y[mcols(y)$keep,] 
-  set.seed(1)
-  y <- swish(y, x="allele", pair="sample")
-  mcols(y)$keep <- NULL
-  write.table(mcols(y), file=paste0("res/bb_",t,".tsv"), sep="\t",quote=FALSE)
+  y <- y[mcols(y)$keep,]
+  n <- ncol(y)/2
+  cts <- assay(y)
+  
+  theta.hat <- 10
+  x <- matrix(rep(1,n),ncol=1)
+  param <- cbind(theta.hat, cts)
+  mle <- rowMeans(ase.cts/cts)
+  beta.hat <- log(mle / (1 - mle))
+  theta.hat <- bbEstDisp(success=ase.cts, size=cts,
+                         x=x, beta=beta.hat,
+                         minDisp=1, maxDisp=50)
+  param <- cbind(theta.hat, cts)
+  fit <- apeglm(Y=ase.cts, x=x, log.lik=NULL, param=param,
+                no.shrink=TRUE, log.link=FALSE, method="betabinCR")
+  mle <- cbind(fit$map[,1], fit$sd[,1])
+  z <- mle[,1]/mle[,2]
+  fit <- locfdr(z)
+  write.table(..., file=paste0("res/bb_",t,".tsv"), sep="\t",quote=FALSE)
 }
