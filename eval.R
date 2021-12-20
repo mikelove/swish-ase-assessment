7dfe0e5ea7cdb673b09b8ca669219aa5f86c68a5
mikelove <michaelisaiahlove@gmail.com>
2021-12-20 07:28:21 -0500

    more work on wasp

--- eval.R
+++ eval.R
@@ -50,12 +50,29 @@ if (wasp) {
   wasp <- read.table("../ase-sim/wasp_cht/cht_results.txt", header=TRUE)
   wasp <- wasp %>% mutate(ratio=ALT.AS.READ.COUNT/REF.AS.READ.COUNT,
                   total=TOTAL.AS.READ.COUNT,
-                  pvalue=P.VALUE)
+                  pvalue=P.VALUE,
+                  chr_loc=paste0(TEST.SNP.CHROM,"-",TEST.SNP.POS)) %>%
+    tibble()
   wasp %>% filter(total > 50 & total < 1e5) %>%
     ggplot(aes(total, log2(ratio), col=-log10(pvalue+1e-15))) +
     geom_point() + ylim(-.5, .5)
+  library(GenomicRanges)
   load("../ase-sim/granges.rda")
+  dat <- data.frame(chr=seqnames(txps),
+                    gene_id=txps$gene_id,
+                    txp=names(txps),
+                    loc=sapply(txps$snp_loc, min))
+  dat <- dat %>% mutate(chr_loc=paste0(chr, "-", loc))
+  wasp_qval <- wasp %>% inner_join(dat, by="chr_loc") %>%
+    distinct(gene_id, pvalue) %>%
+    mutate(qvalue=p.adjust(pvalue, method="BH"))
+  # fix this...
+  wasp_qval <- wasp_qval %>% filter(!duplicated(gene_id)) %>%
+    inner_join(dat) %>% select(qvalue, txp)
+  padj$wasp <- 1
+  padj[wasp_qval$txp,"wasp"] <- wasp_qval$qvalue
 }
+
 cd <- COBRAData(padj=padj, truth=truth)
 cp <- calculate_performance(cd,
                             binary_truth="status",
@@ -64,17 +81,24 @@ cp <- calculate_performance(cd,
                             splv="group",
                             thrs=c(.01,.05,.1),
                             thr_venn=.05)
+cpnf <- calculate_performance(cd,
+                              binary_truth="status",
+                              aspect=c("tpr","fdr","fdrtpr","fdrnbr",
+                                       "fdrtprcurve","overlap"),
+                              thrs=c(.01,.05,.1),
+                              thr_venn=.05)
 
-cp <- prepare_data_for_plot(cp, colorscheme=cols)
+cplot <- prepare_data_for_plot(cp, colorscheme=cols)
+cplotnf <- prepare_data_for_plot(cpnf, colorscheme=cols)
 
 # simple plot
-plot_tpr(cp, pointsize=2.5)
+plot_tpr(cplot, pointsize=2.5)
 
 # TPR over FDR
 yrng <- c(0,1)
-xrng <- c(0,.15)
-#xrng <- c(0,.2)
-plot_fdrtprcurve(cp,
+#xrng <- c(0,.15)
+xrng <- c(0,.5)
+plot_fdrtprcurve(cplotnf,
                  plottype="points",
                  xaxisrange=xrng,
                  yaxisrange=yrng,
@@ -82,9 +106,9 @@ plot_fdrtprcurve(cp,
 
 if (FALSE) {
   # FDR and number
-  plot_fdrnbrcurve(cp, xaxisrange=xrng) +
+  plot_fdrnbrcurve(cplotnf, xaxisrange=xrng) +
     ggplot2::ylim(0,4500)
   
   # upset plot
-  plot_upset(cp, order.by="freq", nintersects=10)
+  plot_upset(cplotnf, order.by="freq", nintersects=10)
 }
