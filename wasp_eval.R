973c93cb8da096260a7938186da6a66832f3cf3d
mikelove <michaelisaiahlove@gmail.com>
2021-12-21 06:51:51 -0500

    work on wasp

--- wasp_eval.R
+++ wasp_eval.R
@@ -6,28 +6,48 @@ names(cols)[6] <- "wasp"
 
 library(dplyr)
 library(ggplot2)
+library(plyranges)
 wasp <- read.table("../ase-sim/wasp_cht/cht_results.txt", header=TRUE)
-wasp <- wasp %>% mutate(ratio=ALT.AS.READ.COUNT/REF.AS.READ.COUNT,
-                        total=TOTAL.AS.READ.COUNT,
-                        pvalue=P.VALUE,
-                        chr_loc=paste0(TEST.SNP.CHROM,"-",TEST.SNP.POS)) %>%
-  tibble()
-wasp %>% filter(total > 50 & total < 1e5) %>%
-  ggplot(aes(total, log2(ratio), col=-log10(pvalue+1e-15))) +
-  geom_point() + ylim(-.5, .5)
-library(GenomicRanges)
+wasp <- wasp %>% mutate(
+                   start=as.numeric(sapply(strsplit(REGION.START,";"), head, 1)),
+                   end=as.numeric(sapply(strsplit(REGION.END,";"), tail, 1)),
+                   ratio=ALT.AS.READ.COUNT/REF.AS.READ.COUNT) %>%
+  select(seqnames=TEST.SNP.CHROM, start, end,
+         snp=TEST.SNP.POS, ratio,
+         total=TOTAL.AS.READ.COUNT, pvalue=P.VALUE) %>%
+  as_granges()
+
+# plot
+## wasp %>% filter(total > 50 & total < 1e5) %>%
+##   as.data.frame() %>%
+##   ggplot(aes(total, log2(ratio), col=-log10(pvalue+1e-15))) +
+##   geom_point() + coord_cartesian(ylim=c(-.5, .5))
+
+# load truth to identify gene from WASP SNP-level results
+library(plyranges)
 load("../ase-sim/granges.rda")
-dat <- data.frame(chr=seqnames(txps),
-                  gene_id=txps$gene_id,
-                  txp=names(txps),
-                  loc=sapply(txps$snp_loc, min))
-dat <- dat %>% mutate(chr_loc=paste0(chr, "-", loc))
-wasp_qval <- wasp %>% inner_join(dat, by="chr_loc") %>%
+g <- g %>% select(gene_id, isoAI, geneAI)
+wasp <- wasp %>% join_overlap_left_within(g)
+stopifnot(all(!is.na(wasp$gene_id)))
+sum(duplicated(wasp$gene_id))
+
+wasp %>% as.data.frame() %>%
+  mutate(AI=factor(isoAI | geneAI, levels=c("TRUE","FALSE"))) %>% 
+  ggplot(aes(x=pvalue, fill=AI)) + geom_histogram() +
+  ggtitle("WASP gene-level p-values")
+
+wasp_qval <- wasp %>% select(gene_id, pvalue, .drop_ranges=TRUE) %>%
+  as.data.frame() %>% tibble()
+
+###
+
   distinct(gene_id, pvalue) %>%
   mutate(qvalue=p.adjust(pvalue, method="BH"))
-# fix this...
 wasp_qval <- wasp_qval %>% filter(!duplicated(gene_id)) %>%
   inner_join(dat) %>% select(qvalue, txp)
+
+# need to run eval.R script to build 'padj'
+
 padj$wasp <- 1
 padj[wasp_qval$txp,"wasp"] <- wasp_qval$qvalue
 
